84ccc4b0175fb67f71656384bd8522867abe6910
GiovanniBombardieri <g.bombardieri06@gmail.com>
2025-01-17 14:20:59 +0100

    :boom: (dev) Try Material Table for display all fruits

--- src/app/components/all-fruits/all-fruits.component.ts
+++ src/app/components/all-fruits/all-fruits.component.ts
@@ -7,11 +7,28 @@ import { fruitDescription } from "../../models/fruit-data";
 import { MatCardModule } from "@angular/material/card";
 import { MatDividerModule } from "@angular/material/divider";
 import { MatGridListModule } from "@angular/material/grid-list";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatPaginatorModule } from "@angular/material/paginator";
+
+import { MatTableDataSource, MatTableModule } from "@angular/material/table";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 @Component({
   selector: "app-all-fruits",
   standalone: true,
-  imports: [MatCardModule, MatDividerModule, MatGridListModule],
+  imports: [
+    MatCardModule,
+    MatDividerModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+  ],
   templateUrl: "./all-fruits.component.html",
   styleUrl: "./all-fruits.component.scss",
 })
@@ -20,6 +37,28 @@ export class AllFruitsComponent implements OnInit {
   fruitsNames: string[] = [];
   fruitData = fruitDescription;
   isHovered: boolean = false;
+  showContentCard: { [key: number]: boolean } = {};
+
+  // TABLE
+  displayedColumns: string[] = [
+    "img",
+    "name",
+    "family",
+    "order",
+    "genus",
+    // "plus",
+    // "calories",
+    // "carbohydrates",
+    // "fat",
+    // "protein",
+    // "sugar",
+  ];
+  dataSource: any;
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
   constructor(private apiService: ApiService) {}
 
@@ -27,10 +66,14 @@ export class AllFruitsComponent implements OnInit {
     this.seeAllFruits();
   }
 
+  toggleContentCard(fruitId: number): void {
+    this.showContentCard[fruitId] = !this.showContentCard[fruitId];
+  }
+
   seeAllFruits(): void {
     this.apiService.getAllFruits().subscribe((data: Fruit[]) => {
       this.fruits = data;
-      console.log(this.fruits);
+      this.dataSource = new MatTableDataSource(data);
 
       this.fruits.forEach((value) => {
         this.fruitsNames.push(value.name);
@@ -38,11 +81,11 @@ export class AllFruitsComponent implements OnInit {
     });
   }
 
-  checkFruitColor(): void {
-    Object.entries(this.fruitData).forEach(([key, value]) => {
-      if (this.fruitsNames.includes(key)) {
-        console.log(key);
-      }
-    });
-  }
+  // checkFruitColor(): void {
+  //   Object.entries(this.fruitData).forEach(([key, value]) => {
+  //     if (this.fruitsNames.includes(key)) {
+  //       console.log(key);
+  //     }
+  //   });
+  // }
 }
